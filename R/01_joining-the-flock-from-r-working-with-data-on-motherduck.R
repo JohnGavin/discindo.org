878e6aadfc46048dced30183622e21046b529460
John Gavin <john.b.gavin@gmail.com>
2024-08-06 10:03:02 +0100

    tidying up

--- R/01_joining-the-flock-from-r-working-with-data-on-motherduck.R
+++ R/01_joining-the-flock-from-r-working-with-data-on-motherduck.R
@@ -2,6 +2,9 @@
 # https://discindo.org/post/shiny-ducks-connecting-to-motherduck-from-shiny/
 # https://app.motherduck.com/
 
+Sys.setenv('MD_TOKEN'='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXNzaW9uIjoiam9obi5iLmdhdmluK2dpdGh1Yi5nbWFpbC5jb20iLCJlbWFpbCI6ImpvaG4uYi5nYXZpbitnaXRodWJAZ21haWwuY29tIiwidXNlcklkIjoiOTViMjgzMDctYzI0NS00ZmE5LThkZGUtNzcyMjUyNjliZGU0IiwiaWF0IjoxNzE5NjkwOTQ3LCJleHAiOjE3NTEyNDg1NDd9.3u7LKEEw0d-4KZ4gw1rcDppA_CMKC042r79nemOZT4I')
+Sys.getenv('MD_TOKEN')
+
 if (!requireNamespace('pak')) installed.packages('pak')
 if (!requireNamespace('pacman')) pak::pak(pacman)
 pacman::p_load(DBI, duckdb)
